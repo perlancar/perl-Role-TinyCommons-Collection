2e5348b1e3279e79edc66a2297195feee902fd68
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-02 19:39:36 +0700

    Add Role::TinyCommons::Collection::CompareItems

--- lib/Role/TinyCommons/Collection/FindItem/Iterator.pm
+++ lib/Role/TinyCommons/Collection/FindItem/Iterator.pm
@@ -15,7 +15,7 @@ with 'Role::TinyCommons::Collection::FindItem';
 ### requires
 
 requires 'each_item';
-requires 'cmp_item';
+requires 'cmp_items'; # Role::TinyCommons::Collection::CompareItems
 
 ### provides
 
@@ -30,7 +30,7 @@ sub find_item {
     $self->each_item(
         sub {
             my ($iter_item, $obj, $pos) = @_;
-            if ($obj->cmp_item($iter_item, $search_item) == 0) {
+            if ($obj->cmp_items($iter_item, $search_item) == 0) {
                 push @results, $return_pos ? $pos : $iter_item;
                 return 0 unless $all;
             }
@@ -45,7 +45,7 @@ sub find_item {
 =head1 DESCRIPTION
 
 This role provides find_item() which searches linearly using each_item() and
-cmp_item(). each_item() is usually provided by an iterator.
+cmp_items(). each_item() is usually provided by an iterator.
 
 
 =head1 ROLES MIXED IN
@@ -60,14 +60,16 @@ L<Role::TinyCommons::Collection::FindItem>
 Provided by roles like in L<Role::TinyCommons::Iterator::Resettable> or
 L<Role::TinyCommons::Iterator::Circular>.
 
-=head2 cmp_item
+=head2 cmp_items
 
 Usage:
 
- $res = $obj->cmp_item($item1, $item2); # returns either -1, 0, 1
+ $res = $obj->cmp_items($item1, $item2); # returns either -1, 0, 1
 
 For flexibility in searching. The method should accept two items and return
-either -1, 0, 1 like Perl's C<cmp> or C<< <=> >> operator.
+either -1, 0, 1 like Perl's C<cmp> or C<< <=> >> operator. See also
+L<Role::TinyCommons::Collection::CompareItems> for the more formal encapsulated
+form of this interface.
 
 
 =head1 PROVIDED METHODS
